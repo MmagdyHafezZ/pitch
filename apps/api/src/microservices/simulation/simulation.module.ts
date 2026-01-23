a0491bbff668f8d7055957eb4a2cc170c95b21f3
ymamoun <youssef.mamoun@ucalgary.ca>
2026-01-22 17:01:51 -0700

    fix: undo

--- apps/api/src/microservices/simulation/simulation.module.ts
+++ apps/api/src/microservices/simulation/simulation.module.ts
@@ -1,16 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TtsModule } from './tts/tts.module';
-import { JwtModule } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
+
 @Module({
-  imports: [
-    PassportModule,
-    JwtModule.register({
-      secret: process.env.JWT_SECRET || 'your-secret-key',
-      signOptions: { expiresIn: '150m' },
-    }),
-    TtsModule,
-  ],
+  imports: [TtsModule],
   providers: [],
   exports: [TtsModule],
 })
