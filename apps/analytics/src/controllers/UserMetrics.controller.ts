897ee67ba235a8d03302f97fd943a048dbdb2ad0
Magdy-Hafez#030901 <Magdy.hafez@ucalgary.ca>
2026-03-10 05:30:10 -0600

    chore(api): remove misplaced standalone analytics app

--- apps/analytics/src/controllers/UserMetrics.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common'
-
-@Controller('user-metrics')
-export class UserMetricsController {
-  @Get()
-  getUserMetrics() {
-    return [{ id: 1, name: 'John Doe', metrics: { visits: 10, clicks: 20 } }]
-  }
-}
